42d20f0e3b982c6047886d087f86de5503139a91
yuma14 <yuma140902@gmail.com>
2025-05-13 11:45:52 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+	description = "AtCoder";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/24.11";
+		utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+		"x86_64-linux"
+	] (system: let
+			pkgs = import nixpkgs {
+				inherit system;
+				config.allowUnfree = true;
+			}; 
+		in {
+			devShells.default = pkgs.mkShell rec {
+				name = "atcoder";
+				packages = with pkgs; [
+					clang_16
+					gcc12
+				];
+			};
+		});
+}
